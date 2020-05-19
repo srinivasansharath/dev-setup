d2a1347cd1739397704709b07dd7299d484c20c7
Sharath Srinivasan <sharath.srinivasan@intel.com>
2020-05-19 09:25:42 +0000

    removed unwanted spaces

--- install.sh
+++ install.sh
@@ -21,35 +21,32 @@ export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 
 
 # setup the new .vimrc file                                                 
-cd $CURRENTDIR                                                              
-pwd                                                                         
-sudo cp ~/.vimrc ~/.vimrc_backup                                            
-sudo chown $USER:$GROUP ~/.vimrc_backup                                     
-pwd                                                                         
-sudo cp ./vimrc ~/.vimrc                                                    
-sudo chown $USER:$GROUP ~/.vimrc                                            
-                                                                            
-# create the undo directory to store undo content                           
-sudo mkdir -p ~/.vim/undodir                                                
-sudo chown $USER:$GROUP -R ~/.vim                                           
-                                                                            
-# download the vim-plug plugin                                              
-sudo mkdir -p ~/.vim/autoload/                                              
-sudo chown $USER:$GROUP -R ~/.vim/autoload                                  
-                                                                            
-sudo curl -fLo ~/.vim/autoload/plug.vim --create-dirs \                     
-            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim 
-sudo chown $USER:$GROUP -R ~/.vim/autoload/plug.vim                         
-                                                                            
-# download and install all the vim-plug plugins                             
-vim +'PlugInstall --sync' +qa                                               
-                                                                            
-# build the YouCompleteMe bundle                                            
-#cd ~/.vim/plugged/YouCompleteMe/                                           
-#python3 install.py --all                                                   
-                                                                            
-cd $CURRENTDIR                                                              
-                                                                            
+cd $CURRENTDIR 
+sudo cp ~/.vimrc ~/.vimrc_backup 
+sudo chown $USER:$GROUP ~/.vimrc_backup 
+sudo cp ./vimrc ~/.vimrc 
+sudo chown $USER:$GROUP ~/.vimrc 
+
+# create the undo directory to store undo content
+sudo mkdir -p ~/.vim/undodir
+sudo chown $USER:$GROUP -R ~/.vim 
+
+# download the vim-plug plugin
+sudo mkdir -p ~/.vim/autoload/
+sudo chown $USER:$GROUP -R ~/.vim/autoload
+
+sudo curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sudo chown $USER:$GROUP -R ~/.vim/autoload/plug.vim
+
+# download and install all the vim-plug plugins
+vim +'PlugInstall --sync' +qa
+
+# build the YouCompleteMe bundle
+#cd ~/.vim/plugged/YouCompleteMe/
+#python3 install.py --all
+
+cd $CURRENTDIR
+
 echo 'colorscheme gruvbox' >> ~/.vimrc
 sudo chmod 777 /tmp/vim-rtags-python.log
 
