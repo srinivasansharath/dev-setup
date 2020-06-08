1f2a5791f7d22790b7d6df4eb9ffa257eb360524
Sharath Srinivasan <sharath.srinivasan@intel.com>
2020-06-08 04:24:15 +0000

    updates to test colors

--- test_term_colors.sh
+++ test_term_colors.sh
@@ -5,14 +5,25 @@
 ## Author: qeatzy
 
 ## Background colors
+echo
+echo  Background colors
+echo
+
 for((i=16; i<256; i++)); do
     printf "\e[48;5;${i}m%03d" $i;
     printf '\e[0m';
-    [ ! $((($i - 15) % 6)) -eq 0 ] && printf ' ' || printf '\n'
+    [ ! $((($i - 15) % 18)) -eq 0 ] && printf ' ' || printf '\n'
 done
+echo
 
 ## Foregroud colors
+echo
+echo Foreground colors
+echo
+
 for i in {0..255}; 
   do printf '\e[38;5;%dm%3d ' $i $i;
   (((i+3) % 18)) || printf '\e[0m\n';
 done
+echo
+echo
